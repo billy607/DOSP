d542f257176303e119cd94e37523bf64fc65cdee
Enyang Wang <billywang19960607@gmal.com>
2019-09-07 19:49:36 -0400

    p1 final final final

--- project1/proj1.exs
+++ project1/proj1.exs
@@ -9,20 +9,23 @@ end
 args = System.argv()
 n1 = String.to_integer(Enum.at(args,0))
 n2 = String.to_integer(Enum.at(args,1))
-range1 = div(n1,1000)
-range2 = div(n2,1000)
 pid = self()
 if n1<10 do
-    if n2<10 do
-    else 
-        n1=10
-		Boss.start_link(range2-range1,pid,n1,n2)
+	if n2<10 do
+		send self(),{:finish}
+	else 
+		n1 = 10
+		if rem((n2-n1+1),1000) != 0 do
+			Boss.start_link(div((n2-n1+1),1000)+1,pid,n1,n2)
+		else
+			Boss.start_link(div((n2-n1+1),1000),pid,n1,n2)
+		end
     end
 else
-	if rem(n1,1000) != 0 do
-		Boss.start_link(range2-range1,pid,n1,n2)
+	if rem((n2-n1+1),1000) != 0 do
+		Boss.start_link(div((n2-n1+1),1000)+1,pid,n1,n2)
 	else
-		Boss.start_link(range2-range1+1,pid,n1,n2)
+		Boss.start_link(div((n2-n1+1),1000),pid,n1,n2)
     end
 end
 Wait.waitSignal
