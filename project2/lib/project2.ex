fda1d3a9ff8619e76a7ca8266d9125b963796c57
Ziqi Liu <1459665333@qq.com>
2019-09-24 13:42:20 -0400

    add receive module to main function, finish gossip full

--- project2/lib/project2.ex
+++ project2/lib/project2.ex
@@ -1,24 +1,40 @@
 defmodule Pro2 do
 	def main(nNum,topology,algorithm) do
+		Process.register(self(),:main)
 		case algorithm do
 			"gossip"->Pro2.gossip(nNum,topology)
 			"push_sum"->Pro2.push_sum(nNum,topology)
 		end
-		:timer.sleep(5000)
+		waitSignal(nNum)
 	end 
+	def waitSignal(n) when n==0 do
+		IO.puts("finish")
+	end
+	def waitSignal(n) do
+		IO.puts(n)
+		receive do
+			{:finish}->waitSignal(n-1)
+		end
+	end
+	
 #############################################algorithm
 	def gossip(nNum,topology) do
 		IO.puts("gossip")
 		list=Enum.to_list 1..nNum
 		plist=[]
 		plist=plist++Enum.map(list,fn(x)->
-			{:ok, pid}=Node.start_link(plist)
+			{:ok, pid}=Node.start_link(plist,0,0,0)
 			pid
 		end)
 		Enum.map(plist,fn(x)->
-			neighbor=full(x,plist)
-			IO.inspect(neighbor)
-			Node.update(x,neighbor)
+			case topology do
+				"full"->Node.update(x,full(x,plist))
+				"line"->IO.puts(" ")
+				"rand2D"->IO.puts(" ")
+				"torus"->IO.puts(" ")
+				"honeycomb"->IO.puts(" ")
+				"ranhoneycomb"->IO.puts(" ")
+			end
 		end)
 		first=List.first(plist)
 		Node.send(first)
@@ -30,8 +46,7 @@ defmodule Pro2 do
 		
 ###########################################topology
 	def full(pid,plist) do
-		plist=plist--[pid]		
-		plist++[0]
+		plist=plist--[pid]
 	end
 	def line() do
 	
@@ -52,11 +67,11 @@ end
 ##########################################genserver Node
 defmodule Node do
 	use GenServer
-	def init(neighbor) do
-		{:ok,neighbor}
+	def init(list) do
+		{:ok,list}
 	end
-	def start_link(neighbor) do
-		GenServer.start_link(__MODULE__,neighbor)
+	def start_link(neighbor,s,w,times) do
+		GenServer.start_link(__MODULE__,[neighbor,s,w,times])
 	end
 	def update(pid,newneighbor) do
 		GenServer.cast(pid, {:update, newneighbor})
@@ -72,41 +87,44 @@ defmodule Node do
 	end
 	
 	
-	def handle_cast({:update,newneighbor},neighbor) do
-		neighbor=neighbor++newneighbor
-		{:noreply, neighbor}
+	def handle_cast({:update,newneighbor},list) do
+		neighbor=hd(list)++newneighbor
+		list=List.replace_at(list,0,neighbor)
+		{:noreply, list}
 	end	
-	def handle_cast({:update_minus,newneighbor},neighbor) do
-		IO.puts("hi")
-		neighbor=neighbor--[newneighbor] 
-		if List.first(neighbor)==List.last(neighbor) do 
-			IO.inspect(neighbor++self())
+	def handle_cast({:update_minus,newneighbor},list) do
+		neighbor=hd(list)--[newneighbor] 
+		list=List.replace_at(list,0,neighbor)
+		if Enum.empty?(neighbor) do 
+			IO.inspect(self(), label: "done")
+			send :main,{:finish}
 			Process.exit(self(),:normal) 
 		end
-		{:noreply, neighbor}
+		{:noreply, list}
 	end
-	def handle_cast({:send},neighbor) do
-		last=List.last(neighbor)
-		des=Enum.random(neighbor--[last])
+	def handle_cast({:send},list) do
+		#last=List.last(list)
+		des=Enum.random(hd(list))
 		Node.receive(des,"hello")
-		:timer.sleep(10)
 		Node.send(self())
-		{:noreply, neighbor}
+		{:noreply, list}
 	end
-	def handle_cast({:receive, pid, message},neighbor) do
-		last=List.last(neighbor)
+	def handle_cast({:receive, pid, message},list) do
+		last=List.last(list)
 		last=last+1
-		IO.puts(last)
-		len=length(neighbor)-1
-		neighbor=List.replace_at(neighbor,len,last)
+		#IO.puts(last)
+		list=List.replace_at(list,3,last)
 		if message=="hello" do
 			if last==10 do
-				IO.inspect(neighbor++self())
-				Enum.each(neighbor,fn(x)->Node.update_minus(self(),x)end)
+				#IO.inspect(hd(list)++self())
+				IO.inspect(self(), label: "done")
+				Enum.each(hd(list),fn(x)->Node.update_minus(x,self())end)
+				send :main,{:finish}
+				#IO.inspect("111111")
 				Process.exit(pid,:normal)
 			end
 		end
 		Node.send(pid)
-		{:noreply, neighbor}
+		{:noreply, list}
 	end
 end
