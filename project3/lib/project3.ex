447d8ba7e210416ccdbf7affbce1c2b6e7b90619
Enyang Wang <billywang19960607@gmal.com>
2019-10-23 16:02:06 -0400

    proj3

--- project3/lib/project3.ex
+++ project3/lib/project3.ex
@@ -4,8 +4,8 @@ defmodule TNode do
     (:ok,list)
   end
 
-  def start_link(x,y,nid,guid,neighbor) do
-    GenServer.start_link(__MODULE__,[x,y,nid,guid,neighbor])
+  def start_link(nid,guid,objectMap,neighborMap) do
+    GenServer.start_link(__MODULE__,[nid,guid,objectMap,neighbor,neighborMap])
   end
 
   def publishObj(guid,pid,lvl) do
@@ -13,9 +13,21 @@ defmodule TNode do
   end
 
   def handle_cast({:publish,guid,pid,lvl},list) do
-    neighbor = List.last(list)
-    digit = String.at(guid,lvl-1)   #the vale of guid at level lvl-1
+    #if this is root
+    objMap = Map.put(Enum.at(list,2),String.to_atom(guid),pid)
+    if guid == Enum.at(list,1)||lvl > 4 do
+      {:noreply,List.replace_at(list,2,objMap)}
+    end
+    neighborMap = List.last(list)
     
+    #digit = String.at(guid,lvl-1)   #the vale of guid at level lvl
+    #nextN = Enum.at(Enum.at(neighborMap,lvl-1),elem(Integer.parse(digit,16),0)-1)
+    #if nextN == "NULL" do
+    #  allNeighbor = Enum.at(neighborMap,lvl-1)
+    #  nextN = Enum.min_by(allNeighbor,fn x -> abs(elem(Integer.parse(x,16),0)-elem(Integer.parse(guid,16),0)) end)
+    #end
+    #lvl = lvl + 1
+
   end
 
   def test(nNode,nReq) do
